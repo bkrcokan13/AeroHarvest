dc829cf1bfb89ce4ee1fc95ac9271640319388d8
Okan Bakırcı <132247985+bkrcokan13@users.noreply.github.com>
2023-09-17 21:56:16 +0300

    Weather code collector and checker created.

--- weather.py
+++ weather.py
@@ -3,7 +3,6 @@ from requests import RequestException
 from bs4 import BeautifulSoup
 
 
-
 class Weather:
 
     # Search Query
@@ -35,27 +34,100 @@ class Weather:
                 timeout=3
             )
 
+            if (weahtherCode.status_code == 403):
+                print("Status : Forbidden, check your agent or change another agent !.")
+            elif (weahtherCode.status_code == 200):
+                print("Status : OK !")
+                self._GetWeatherCode(data=weahtherCode.content)
+                
+                
 
 
             
         except RequestException:
-            print("Error ! ")
+            print("Error, url not working or not founded !")
 
-    def _GetWeatherCode(data):
-        pass
-    
+
+    def checkLocationExists(self,data):
+
+        checkResultText = self._bsSoup.find('div', attrs={
+            'class':'no-results-text'
+        })
+        
+        if checkResultText:
+            
+            if checkResultText.text == "No results found.":
+                return False
+            else:
+                return True
+
+    def _GetWeatherCode(self,data):
+        
+
+        # Weather Dict
+        weatherList = {}
+
+        try:
+            # Bs4 Initlaize 
+            self._bsSoup = BeautifulSoup(data, "html.parser")
+            
+
+            # Check result
+            if self.checkLocationExists(data):
+                print("Location is not found")
+                return weatherList
+            else:
+
+                # Collect Weather Code's 
+                getAllCode = self._bsSoup.find('div', attrs={'class':'locations-list content-module'})
+
+                # Check Value 
+                if getAllCode:
+
+                    # Find div inside all A tag's 
+                    weatherCode = getAllCode.find_all('a')
+
+                    count = 0
+
+                    # Clear prev data's
+                    weatherList.clear()
+                    for code in weatherCode:
+                            
+                        textLocation = code.text.strip()
+                        url = ("https://www.accuweather.com/"+ code.get('href'))
+
+                        count += 1
+
+                        # Convert count Int to Str
+                        urlID = str(count)
+
+                        # Create dict
+                        weatherList[urlID] = {
+                            "location":f"{textLocation}",
+                            "apiUrl": f"{url}"
+                        }
+                    print(f"Location's {len(weatherCode)} found...!")
+                
+                return weatherList
+        except Exception as error:
+            print("Error, weather code's not collected or url is broken !! check url or user-agent !")
+            print(error.with_traceback())
+        
+
+       
+
     
     # Get Weather Status
-    def _WeatherTemp(data):
+    def _WeatherTemp(self, data):
         return ""
     
-    def _AirQuality(data):
+    def _AirQuality(self, data):
         return ""
 
-    def _WindStatus(data):
+    def _WindStatus(self, data):
         return ""
 
-    def _DownloadMap(data):
+    def _DownloadMap(self, data):
         return ""
 
 
@@ -64,5 +136,5 @@ app = Weather()
 
 
 app.SearchLocation(
-    location="Amsterdam"
+    location="Rotterdam"
 )
