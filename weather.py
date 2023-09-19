f65e1c9c31196cd28c8a14c5fb9d1d824d567999
Okan Bakırcı <132247985+bkrcokan13@users.noreply.github.com>
2023-09-20 00:16:29 +0300

    Error's fixed and current weather fixed

--- weather.py
+++ weather.py
@@ -4,14 +4,9 @@ from bs4 import BeautifulSoup
 
 
 class Weather:
-
-    
-    
-
     def __init__(self):
         # Search Query
         self.searchQuery = "https://www.accuweather.com/en/search-locations?query="
-
 
         self.selectedApiUrl = None
 
@@ -26,16 +21,15 @@ class Weather:
 
         # Config header
         self.HEADERS = {
-                'User-Agent': ('Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
-                            'AppleWebKit/537.36 (KHTML, like Gecko)'
-                            'Chrome/116.0.0.0 Safari/537.36'),
-                'Accept-Language': 'en-US, en;q=0.5'
+            'User-Agent': ('Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+                           'AppleWebKit/537.36 (KHTML, like Gecko)'
+                           'Chrome/116.0.0.0 Safari/537.36'),
+            'Accept-Language': 'en-US, en;q=0.5'
         }
 
-
-    def SearchLocation(self ,location):
+    def SearchLocation(self, location):
         try:
-            
+
             print(self.searchQuery + location)
 
             weahtherCode = requests.get(
@@ -45,10 +39,10 @@ class Weather:
             )
 
             # Check status code
-            if (weahtherCode.status_code == 403):
+            if weahtherCode.status_code == 403:
                 print("Status : Forbidden, check your agent or change another agent !.")
 
-            elif (weahtherCode.status_code == 200):
+            elif weahtherCode.status_code == 200:
                 print("Status : OK !")
                 self._get_weather_code(data=weahtherCode.content)
 
@@ -56,26 +50,25 @@ class Weather:
             print("Error, url not working or not founded !")
             print(reqExp.strerror)
 
-
-    def checkLocationExists(self,data):
+    def checkLocationExists(self, data):
 
         checkResultText = self._bsSoup.find('div', attrs={
-            'class':'no-results-text'
+            'class': 'no-results-text'
         })
-        
+
         if checkResultText:
-            
+
             if checkResultText.text == "No results found.":
                 return False
             else:
                 return True
 
-    def _get_weather_code(self,data):
-        
+    def _get_weather_code(self, data):
+
         try:
-            # Bs4 Initlaize 
+            # Bs4 Initialize
             self._bsSoup = BeautifulSoup(data, "html.parser")
-            
+
             # Check result
             if self.checkLocationExists(data):
                 print("Location is not found")
@@ -83,7 +76,7 @@ class Weather:
             else:
 
                 # Collect Weather Code's 
-                getAllCode = self._bsSoup.find('div', attrs={'class':'locations-list content-module'})
+                getAllCode = self._bsSoup.find('div', attrs={'class': 'locations-list content-module'})
 
                 # Check Value 
                 if getAllCode:
@@ -95,10 +88,9 @@ class Weather:
                     # Clear prev data's
                     self.weatherList.clear()
                     for code in weatherCode:
-                        
                         # Clear whitespace
-                        textLocation = code.text.replace(" ", " ").replace("\n", " ").replace("\t","")
-                        url = ("https://www.accuweather.com/"+ code.get('href'))
+                        textLocation = code.text.replace(" ", " ").replace("\n", " ").replace("\t", "")
+                        url = ("https://www.accuweather.com/" + code.get('href'))
 
                         # Add count
                         count += 1
@@ -108,11 +100,11 @@ class Weather:
 
                         # Create dict
                         self.weatherList[urlID] = {
-                            "location":f"{textLocation}",
+                            "location": f"{textLocation}",
                             "apiUrl": f"{url}"
                         }
-                    
-                    # Check Weather List Lenght
+
+                    # Check Weather List Length
                     if len(self.weatherList) != 0:
                         print(f"Location {len(self.weatherList)} founded !.")
                         self.weatherCodeStatus = True
@@ -128,10 +120,8 @@ class Weather:
         while True:
 
             for location in range(len(self.weatherList)):
-            
                 locationName = self.weatherList[str(location + 1)]["location"]
                 print(f"({location + 1}) : {locationName}\n")
-
 
             # Choice User Input
             inputLocation = input("Select Location : ")
@@ -139,28 +129,21 @@ class Weather:
             #
             # print(f"{len(self.weatherList)} - {inputLocation}")
 
-            
-
             # Check list out range
             if int(inputLocation) >= (len(self.weatherList) + 1):
-                
+
                 print("Location is not available...!!!")
                 input("Press any key !")
             else:
-                locationId = str(inputLocation) 
+                locationId = str(inputLocation)
                 break
-        
-       
 
         self.CollectData(id=locationId)
-        
 
     def CollectData(self, id):
 
-
         # Check Page Status
         pageStatus = False
-    
 
         # Get Page
         getWeather = requests.get(self.weatherList[id]["apiUrl"], headers=self.HEADERS)
@@ -170,66 +153,82 @@ class Weather:
             pageStatus = True
         elif getWeather.status_code == 403:
             print("403 Forbidden, check headers !")
-        
 
         if pageStatus:
             try:
                 # Parse weather page
                 soup = BeautifulSoup(getWeather.content, "html.parser")
 
-          
+                tempList, phraseList = [], []
+
+                tempList.clear()
+                phraseList.clear()
 
                 cardContent = soup.find_all(
                     'div', attrs={
-                        'class':'card-content'
+                        'class': 'card-content'
                     }
                 )
 
                 for card in cardContent:
-                    
-                    temp  = card.find('div', attrs={
-                        'class':'temp'
+                    tempData = card.find('div', attrs={
+                        'class': 'temp'
                     })
 
-                    tempData = temp.text.split('\n')
+                    phrase = card.find('div', attrs={
+                        'class': 'phrase'
+                    })
 
+                    tempList.append(tempData.text)
+                    phraseList.append(phrase.text)
 
-                    for data in tempData:
-                        splitData = data.split('°')
+                zip_weather_data = zip(tempList, phraseList)
 
-                        degree = splitData[0]
-                        status = splitData[1]
+                weather_data = list(zip_weather_data)
 
-                        print(degree + "°", status)
+                for hdr in range((len(self.weatherList[id]['location']) * 2)):
+                    print('*', end="")
+                print("\n")
+                print(f"(Selected Location {self.weatherList[id]['location'].upper()})\n")
+                print("\n")
 
-                    
-                    
-                   
+                for dt in weather_data:
+                    print(f"{dt[0].upper()}  -  {dt[1].upper()}")
+
+                print("\n")
+                for hdr in range((len(self.weatherList[id]['location']) * 2)):
+                    print('*', end="")
+
+                # for card in cardContent:
+
+                #     temp  = card.find('div', attrs={
+                #         'class':'temp'
+                #     })
+
+                #     tempData = temp.text.split('\n')
+
+                #     for data in tempData:
+                #         # splitData = data.split('°')
+
+                #         # degree = splitData[0]
+                #         # status = splitData[1]
+
+                #         # print(degree + "°", status)
+
+                #         tempList.append(data)
+                # print(tempList)
 
 
 
             except AttributeError:
                 print("Text is not available !")
             except Exception as errorEx:
-                print("Error, datas not fetched or not found !")
+                print("Error, data not fetched or not found !")
                 errorDetails = f"""
                 Error Traceback :\n\t{errorEx.with_traceback()}\n\t
                 """
 
                 print(errorDetails)
-            
-
-            
-          
-
-            
-
-            
-           
-
-            
-
-            
 
 
 app = Weather()
